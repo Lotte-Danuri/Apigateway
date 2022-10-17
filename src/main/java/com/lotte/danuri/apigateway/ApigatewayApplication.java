8b24ef9374af9b2df58cbbfff2e0de86a8a435f7
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-10-17 18:49:13 +0900

    FEAN-158 [fix] CORS 문제 해결을 위한 빈 등록

--- src/main/java/com/lotte/danuri/apigateway/ApigatewayApplication.java
+++ src/main/java/com/lotte/danuri/apigateway/ApigatewayApplication.java
@@ -1,14 +1,52 @@
 package com.lotte.danuri.apigateway;
 
+import com.lotte.danuri.apigateway.config.PassCorsRoutePredicateHandlerMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
+import org.springframework.cloud.gateway.handler.FilteringWebHandler;
+import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	/*@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.setAllowCredentials(true);
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return source;
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		return new CorsWebFilter(corsConfigurationSource());
+	}*/
+
+	@Bean
+	@Primary
+	public RoutePredicateHandlerMapping passCorsRoutePredicateHandlerMapping(
+		FilteringWebHandler webHandler, RouteLocator routeLocator,
+		GlobalCorsProperties globalCorsProperties, Environment environment) {
+		return new PassCorsRoutePredicateHandlerMapping(webHandler, routeLocator,
+			globalCorsProperties, environment);
 	}
 
 }
